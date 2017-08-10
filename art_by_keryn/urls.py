fbe6792fb980b256811d84b29f5cdf224da2320e
Nicole Peach <nicolepeach84@gmail.com>
2017-08-10 15:26:12 +0100

    First Commit

---
+++ art_by_keryn/urls.py
@@ -0,0 +1,67 @@
+"""art_by_keryn URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from home import views as home_views
+from accounts import views as accounts_views
+from django.conf import settings
+from threads import views as forum_views
+from paypal.standard.ipn import urls as paypal_urls
+from paypal_store import views as paypal_views
+from products import views as product_views
+from django.views.static import serve
+from .settings import MEDIA_ROOT
+
+
+urlpatterns = [
+    # General Page URLs
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', home_views.get_index, name='index'),
+    url(r'^about/', home_views.get_about, name='about'),
+    url(r'^contact/', home_views.get_contact, name='contact'),
+    url(r'^gallery/', home_views.get_gallery, name='gallery'),
+    # Painting URLs
+    url(r'^nelson-mandela/', home_views.get_madiba, name='madiba'),
+    url(r'^mothers-nature/', home_views.get_giraffe, name='giraffe'),
+    url(r'^valli-m/', home_views.get_elephant, name='vallim'),
+    # Account URLs
+    url(r'^register/$', accounts_views.register, name='register'),
+    url(r'^profile/$', accounts_views.profile, name='profile'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
+    # Forum URLs
+    url(r'^forum/$', forum_views.forum, name='forum'),
+    url(r'^threads/(?P<subject_id>\d+)/$', forum_views.threads, name='threads'),
+    url(r'^new_thread/(?P<subject_id>\d+)/$', forum_views.new_thread, name='new_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/$', forum_views.thread, name='thread'),
+    url(r'^post/new/(?P<thread_id>\d+)/$', forum_views.new_post, name='new_post'),
+    url(r'^post/edit/(?P<thread_id>\d+)/(?P<post_id>\d+)/$', forum_views.edit_post, name='edit_post'),
+    url(r'^post/delete/(?P<thread_id>\d+)/(?P<post_id>\d+)/$', forum_views.delete_post, name='delete_post'),
+    url(r'^thread/vote/(?P<thread_id>\d+)/(?P<subject_id>\d+)/$', forum_views.thread_vote, name='cast_vote'),
+    # PayPal URLs
+    url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
+    url(r'^paypal-return', paypal_views.paypal_return),
+    url(r'^paypal-cancel', paypal_views.paypal_cancel),
+    url(r'^products/$', product_views.all_products, name='products'),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
---
+++ home/views.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+
+
+# Create your views here.
+# View for Index Page
+def get_index(request):
+    return render(request, 'index.html')
+
+
+# View for About Page
+def get_about(request):
+    return render(request, 'about.html')
+
+
+# View for Contact Page
+def get_contact(request):
+    return render(request, 'contact.html')
+
+
+# View for Gallery Page
+def get_gallery(request):
+    return render(request, 'gallery.html')
+
+
+# View for Mandela Page
+def get_madiba(request):
+    return render(request, 'nelson-mandela.html')
+
+
+# View for Giraffe Page
+def get_giraffe(request):
+    return render(request, 'giraffe-and-calf.html')
+
+
+# View for Elephant Page
+def get_elephant(request):
+    return render(request, 'vallim-elephant.html')
