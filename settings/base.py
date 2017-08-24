6db7ab21517b4e8fda877fd5daf2d84e21f51434
Nicole Peach <nicolepeach84@gmail.com>
2017-08-24 23:58:37 +0100

    Deployment

--- settings/base.py
+++ settings/base.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'art_by_keryn.urls'
