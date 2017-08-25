d41e3b2678f3c639048fc54d93c8fc37d4d87d16
Nicole Peach <nicolepeach84@gmail.com>
2017-08-25 21:13:10 +0100

    Update Database

--- settings/staging.py
+++ settings/staging.py
@@ -1,15 +1,25 @@
 from base import *
+import dj_database_url
 
 DEBUG = True
 
 INSTALLED_APPS.append('debug_toolbar')
 
+"""
+Local Database Settings
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+# Heroku Database Settings
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
+
 
 SITE_URL = 'art-by-keryn.herokuapp.com'
 
